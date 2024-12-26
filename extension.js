2c0bbce2c3a318c41300851d8ce4cb3f765c4746
Niko Kozak <nikokozak@gmail.com>
2024-12-25 20:36:09 -0500

    Command "Install OpenFrameworks" clones OF github repo into a user-selected folder, and saves location to configuration.

--- extension.js
+++ extension.js
@@ -1,9 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const { exec } = require('child_process');
+const path = require('path');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+// The following will get the path to the OF installation from the workspace settings:
+// const config = vscode.workspace.getConfiguration('easyopenframeworks');
+// const ofPath = config.get('ofPath');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -17,11 +23,48 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('easyopenframeworks.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('easyopenframeworks.installOF', async () => {
+		try {
+			// Get folder to install OF
+			const folder = await vscode.window.showOpenDialog({
+				canSelectFiles: false,
+				canSelectFolders: true,
+				canSelectMany: false,
+				title: 'Select folder to install OpenFrameworks'
+			});
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from EasyOpenFrameworks!');
+			if (!folder) return;
+
+			const targetPath = folder[0].fsPath;
+
+			// Show progress
+			await vscode.window.withProgress({
+				location: vscode.ProgressLocation.Notification,
+				title: "Installing OpenFrameworks",
+				cancellable: false
+			}, async (progress) => {
+				progress.report({ message: 'Cloning repository...' });
+
+				await new Promise((resolve, reject) => {
+					exec(
+						'git clone --recursive git@github.com:openframeworks/openFrameworks.git --depth 1',
+						{ cwd: targetPath },
+						(error) => {
+							if (error) reject(error);
+							else resolve();
+						}
+					);
+				});
+			});
+
+			// Save OF path to workspace settings
+			const config = vscode.workspace.getConfiguration('easyopenframeworks');
+			await config.update('ofPath', path.join(targetPath, 'openFrameworks'), vscode.ConfigurationTarget.Global);
+
+			vscode.window.showInformationMessage('OpenFrameworks installed successfully!');
+		} catch (error) {
+			vscode.window.showErrorMessage(`Failed to install OpenFrameworks: ${error.message}`);
+		}
 	});
 
 	context.subscriptions.push(disposable);
